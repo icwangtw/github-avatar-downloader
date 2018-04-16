47316d4056b03b4291d62833e6c410944bd5a160
Stephen <stephenicwang@gmail.com>
2018-04-16 21:29:39 +0000

    progress1

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,21 @@
 var request = require('request');
+var token = require("./secrets.js")
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, callBack) {
+  var input = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {"User-Agent": "request",
+              "Authorization": token.GITHUB_TOKEN
+    }
+  };
+  request(input, function(error, response, body) {
+    callBack(error, body);
+  });
+}
+
+getRepoContributors("jquery", "jquery", function(error, result) {
+  console.log("Errors:", error);
+  console.log("Result:", result);
+});
