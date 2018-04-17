d4d92d0ab7a51f3c0722efd83211c9e6d74c2a00
Stephen <stephenicwang@gmail.com>
2018-04-17 00:18:45 +0000

    step 10

--- download_avatars.js
+++ download_avatars.js
@@ -1,10 +1,17 @@
 var request = require('request');
-var token = require("./secrets.js")
+var token = require("./secrets.js");
 var fs = require('fs');
 
-var owner = process.argv[2]
-var repo = process.argv[3]
-console.log('Welcome to the GitHub Avatar Downloader!');
+var owner = process.argv[2];
+var repo = process.argv[3];
+
+if (owner === null || repo === null) {
+  console.log("please input both the onwer and repository");
+} else {
+  console.log('Welcome to the GitHub Avatar Downloader!');
+  downloadAvatar();
+};
+
 
 function getRepoContributors(repoOwner, repoName, callBack) {
   var input = {
@@ -16,34 +23,30 @@ function getRepoContributors(repoOwner, repoName, callBack) {
   request(input, function(error, response, body) {
     callBack(error, body);
   });
-}
+};
 
 function downloadImageByURL(url, filePath) {
   request.get(url)
         .on("error", function(error){
           console.log("error!")
         })
-        .on("response", function(response){
-          console.log("Response Status Code: ", response.statusCode);
-        })
-        .on("data", function(){
-          console.log("Image downloading...")
-        })
         .on("end", function(){
           console.log("Image downloaded!")
         })
         .pipe(fs.createWriteStream(filePath))
-}
+};
 
-getRepoContributors(owner, repo, function(error, result) {
-  if (error) {
-    console.log(error)
-  }
-  else {
-    var parsed = JSON.parse(result)
-    fs.mkdir("./avatars")
-    parsed.forEach(function(i){
-      downloadImageByURL(i.avatar_url, "./avatars/" + i.login + ".jpg")
-    })
-  }
-});
+function downloadAvatar() {
+  getRepoContributors(owner, repo, function(error, result) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+      var parsed = JSON.parse(result);
+      fs.mkdirSync("./avatars");
+      parsed.forEach(function(person){
+        downloadImageByURL(person.avatar_url, "./avatars/" + person.login + ".jpg")
+      });
+    };
+  });
+};
